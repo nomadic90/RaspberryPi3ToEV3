3b5e660dadadca50347abc5506655a44bb3944d6
Hyunjoon Ryu <nomadic90@gmail.com>
2018-09-10 23:48:41 +0900

    almost done

--- testSocket.py
+++ testSocket.py
@@ -16,4 +16,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             data = conn.recv(1024)
             # if not data: break
-            print(data)
+            recvData = data.decode()
+            splitData = recvData.split(",")
+            direction = splitData[0]
+            value = int(splitData[1])
+
+            print(direction value)
