709306bbe31ac5c6ad431268648433f3edf1be5a
Hyunjoon Ryu <nomadic90@gmail.com>
2018-09-10 21:50:23 +0900

    add socket receiver

---
+++ testSocket.py
@@ -0,0 +1,18 @@
+import socket
+import serial
+import time
+import EV3BT
+
+HOST = ''
+PORT = 50007
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            print(data)
