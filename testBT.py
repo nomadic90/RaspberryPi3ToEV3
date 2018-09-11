24b62a15ee7b431daf1d6561d41a331f3bd00019
Hyunjoon Ryu <nomadic90@gmail.com>
2018-09-12 00:32:16 +0900

    test1 commit

--- testBT.py
+++ testBT.py
@@ -4,7 +4,7 @@ import time
 import EV3BT
 
 EV3 = serial.Serial('/dev/rfcomm0')
-s = EV3BT.encodeMessage(EV3BT.MessageType.Text, 'text', 'Eat responsibly')
+s = EV3BT.encodeMessage(EV3BT.MessageType.Text, 'text', 'right 0.1')
 print(EV3BT.printMessage(s))
 EV3.write(s)
 time.sleep(1)
