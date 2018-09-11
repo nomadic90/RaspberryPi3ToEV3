0bad1dd59fa7a0b196d7c93984a163533b23ca40
Hyunjoon Ryu <nomadic90@gmail.com>
2018-09-12 00:35:16 +0900

    aa

--- testBT.py
+++ testBT.py
@@ -5,6 +5,7 @@ import EV3BT
 
 EV3 = serial.Serial('/dev/rfcomm0')
 s = EV3BT.encodeMessage(EV3BT.MessageType.Text, 'text', 'right 0.1')
+s = EV3BT.encodeMessage(EV3BT.MessageType.Numeric, 'text', 0.1)
 print(EV3BT.printMessage(s))
 EV3.write(s)
 time.sleep(1)
