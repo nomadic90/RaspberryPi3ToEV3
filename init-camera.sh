24d81b4f4b9f1fecba932f71866443fd5f43997d
hyuryu <nomadic90@gmail.com>
2018-09-11 06:45:03 +0000

    update init

--- init-camera.sh
+++ init-camera.sh
@@ -3,3 +3,5 @@ sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
 sudo apt-get install libxvidcore-dev libx264-dev -y
 sudo apt-get install libgtk2.0-dev -y
 sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install libqtgui4
+sudo apt-get install libqt4-test
