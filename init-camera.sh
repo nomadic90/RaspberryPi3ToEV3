ae40c29f3c5f21937f31b5445a8000c2daf29d0b
hyuryu <nomadic90@gmail.com>
2018-09-11 06:34:03 +0000

    add shell

---
+++ init-camera.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libgtk2.0-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
---
+++ start-camera.sh
@@ -0,0 +1,2 @@
+sudo modprobe bcm2835-v4l2
+python3 streaming-server/Main.py
